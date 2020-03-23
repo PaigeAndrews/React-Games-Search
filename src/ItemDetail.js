3cce64ca88af46f75f97e90d75382433c6e5f686
Paige Andrews <pandrews25@yahoo.com>
2020-03-23 12:55:59 -0400

    added publisher and genres to itemDetail page

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -9,10 +9,10 @@ function ItemDetail({match}) {
       console.log(match)
     }, []);
    
-    useEffect(() => {
-      fetchItem();
-      console.log("paw")
-    }, []);
+    // useEffect(() => {
+    //   fetchItem();
+    //   console.log("paw")
+    // }, []);
    
    
 
@@ -20,6 +20,8 @@ function ItemDetail({match}) {
     const [description, setDescription] = useState("");
     const [video, setVideo] = useState("");
     const [platforms, setPlatforms] = useState([]);
+    const [publisher, setPublisher] = useState("");
+    const [genres, setGenres] = useState([]);
 
     const fetchItem = async () => {
     const fetchItem = await fetch(`https://api.rawg.io/api/games/${match.params.id}`)       
@@ -27,11 +29,15 @@ function ItemDetail({match}) {
     const description = item.description.replace(/<\/p>/g, ' ').replace(/<p>/g, ' ').replace(/<br \/>/g, ' ')
     const video = item.clip.clip
     const platforms = item.platforms.map(x => x.platform.name + " ")
+    const publisher = item.publishers[0].name
+    const genres = item.genres.map(x => x.name + " ")
     setItem(item)
     console.log(item)
     setDescription(description)
     setVideo(video)
     setPlatforms(platforms)
+    setPublisher(publisher)
+    setGenres(genres)
     }
 
 
@@ -46,11 +52,14 @@ function ItemDetail({match}) {
         </iframe>
         {/* <h2> Platforms: {item.platforms.map(x => x.platform.name)}</h2> */}
          {/* {item.ratings[0].count}  */}
-         {/* {item.publishers[0].id} */}
+         {publisher}
          {/* // for (x of items.name) { */}
-      {/* //   return items.name.substring(0, 14) + "...";
+      {/* //   return items.na
+      me.substring(0, 14) + "...";
       //   } */}
+      <div className="genres">{genres.map(genre => { return(<div className="genreEach">{genre}</div>)}) }</div>
       <div className="platforms">{platforms.map(plat => { return(<div className="platformEach">{plat}</div>)}) }</div>
+      
       <div className="itemPara">{description}</div>
     </div>
   );
