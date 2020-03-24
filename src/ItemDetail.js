c3bbdbbb88ee6ee0e23bb39493d1de64536041b7
Paige Andrews <pandrews25@yahoo.com>
2020-03-24 14:21:02 -0400

    added some extra information on itemDetail screen

--- src/ItemDetail.js
+++ src/ItemDetail.js
@@ -17,26 +17,27 @@ function ItemDetail({match}) {
    
 
     const [item, setItem] = useState({});
-    const [description, setDescription] = useState("");
     const [video, setVideo] = useState("");
     const [platforms, setPlatforms] = useState([]);
     const [publisher, setPublisher] = useState("");
     const [genres, setGenres] = useState([]);
+    const [rating, setRating] = useState("");
     const [error, setError] = useState(false);
+    
 
     const fetchItem = async () => {
       setError(false);
       try {
     const fetchItem = await fetch(`https://api.rawg.io/api/games/${match.params.id}`)       
     const item = await fetchItem.json();
-    const description = item.description.replace(/<[^>]+>/g, '');
+    // const description = item.description.replace(/<[^>]+>/g, '');
     setItem(item)
     console.log(item)
-    setDescription(description)
     setVideo(item.clip.clip)
     setPlatforms(item.platforms.map(x => x.platform.name + " "))
     setPublisher(item.publishers[0].name)
     setGenres(item.genres.map(x => x.name + " "))
+    setRating(item.esrb_rating.name)
       }
       catch(error){
         setError(true);
@@ -50,19 +51,21 @@ function ItemDetail({match}) {
         <h1>{item.name}</h1>
         <h2>Released: {item.released}</h2>
         <img alt='' src={item.background_image} className="images"/>
-       
+        <div className="publisher">{publisher}</div>
+        {rating}
+         
         <iframe className="videoClip"
           src={video}>
         </iframe>
         {
-          error && <div className="errorMessage">No Video to display</div>
+          error && <div className="errorMessage">No Content to display</div>
         }
-         {publisher}
-         
+        
+    
       <div className="genres">{genres.map(genre => { return(<div className="genreEach">{genre}</div>)}) }</div>
       <div className="platforms">{platforms.map(plat => { return(<div className="platformEach">{plat}</div>)}) }</div>
       
-      <div className="itemPara">{description}</div>
+      <div className="itemPara">{item.description_raw}</div>
       {
         error && <div className="errorMessage">No additional data on this title</div>
       }
