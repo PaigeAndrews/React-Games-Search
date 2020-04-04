5264ea3642e42ddda1aa37b65fa6ee433398a5d1
Paige Andrews <pandrews25@yahoo.com>
2020-04-04 15:49:54 -0400

    Updates

---
+++ precache-manifest.3ea6f413649f6ed222580bc94d560c47.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527165a431b3240894f230ad98e7ff15",
+    "url": "/React-Games-Search/index.html"
+  },
+  {
+    "revision": "696a78941f49935d85fb",
+    "url": "/React-Games-Search/static/css/2.b328fd89.chunk.css"
+  },
+  {
+    "revision": "ec1b9b07e21120911687",
+    "url": "/React-Games-Search/static/css/main.a5d78a6e.chunk.css"
+  },
+  {
+    "revision": "696a78941f49935d85fb",
+    "url": "/React-Games-Search/static/js/2.b738f9a7.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/React-Games-Search/static/js/2.b738f9a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec1b9b07e21120911687",
+    "url": "/React-Games-Search/static/js/main.c6bb41cc.chunk.js"
+  },
+  {
+    "revision": "834abe4ae686a0ff174d",
+    "url": "/React-Games-Search/static/js/runtime-main.c1b2064b.js"
+  },
+  {
+    "revision": "d929e18e697a2c35a902bec6399b5f6e",
+    "url": "/React-Games-Search/static/media/controllerLogo.d929e18e.png"
+  },
+  {
+    "revision": "413e406da2262dd2765381e49a44d69a",
+    "url": "/React-Games-Search/static/media/random.413e406d.png"
+  }
+]);
